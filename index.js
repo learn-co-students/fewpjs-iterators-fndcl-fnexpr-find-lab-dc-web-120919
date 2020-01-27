14c41e5fab0534042ea45c9c47f0325019e9b181
Mansoor  Cheyo <mcheyo.mc@gmail.com>
2020-01-27 17:08:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
-}
+
+ superbowlWin = (record) => { 
+   let outcome = record.find( record => record.outcome === "W" )
+   return !!outcome ? outcome.year : undefined 
+ }
+
